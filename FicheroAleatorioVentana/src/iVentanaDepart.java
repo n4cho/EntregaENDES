01a606818a4bfe44a551a8083aa3e75cd9982963
Nacho Fernández <ignaciofg@gmail.com>
2022-03-16 14:55:28 +0100

    Commit inicial de esta entrega

---
+++ FicheroAleatorioVentana/src/iVentanaDepart.java
@@ -0,0 +1,13 @@
+import java.awt.event.ActionEvent;
+
+public interface iVentanaDepart {
+
+	void alta();
+
+	void consultas(ActionEvent e);
+
+	void borrar(int dep); // fin borrar
+
+	void modificar(int dep); // fin modificar
+
+}
